e72ed850be212328847fd49deeba4dbabdfa664f
Pawel Piotrowski <pawel@maxlogic.eu>
2025-10-25 20:16:25 +0200

    WIP - 19 tests pass, 4 fail

--- build-and-run-tests.bat
+++ build-and-run-tests.bat
@@ -4,7 +4,7 @@ setlocal
 pushd "%~dp0"
 
 rem Run tests only if the build succeeded (exit code 0)
-call build-delphi.bat tests\MaxEventNexusTests.dproj && ^
+call build-delphi.bat tests\MaxEventNexusTests.dproj -config Debug && ^
 call tests\MaxEventNexusTests.exe
 
 rem Preserve the exit code from whichever ran last (build or tests)
